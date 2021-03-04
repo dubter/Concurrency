142e57672eb7922d3827369ab5fddc89fedaa2f7
Roman Lipovsky <roman.lipovsky@gmail.com>
2021-03-04 15:32:45 +0300

    coroutine task

---
+++ tasks/fibers/coroutine/tests/stacks.cpp
@@ -0,0 +1,25 @@
+#include <mtf/fibers/api.hpp>
+
+#include <wheels/test/test_framework.hpp>
+
+using mtf::fibers::Spawn;
+
+TEST_SUITE(Stacks) {
+  TEST(Pool, wheels::test::TestOptions().TimeLimit(5s).AdaptTLToSanitizer()) {
+    mtf::tp::StaticThreadPool scheduler{1};
+
+    static const size_t kFibers = 1'000'000;
+
+    std::atomic<size_t> counter{0};
+
+    for (size_t i = 0; i < kFibers; ++i) {
+      Spawn([&]() {
+        ++counter;
+      }, scheduler);
+    }
+
+    scheduler.Join();
+
+    ASSERT_EQ(counter, kFibers)
+  }
+}
