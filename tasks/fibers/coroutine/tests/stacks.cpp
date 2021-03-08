6119030cd315c242663c9d6130033285da31ce20
Roman Lipovsky <roman.lipovsky@gmail.com>
2021-03-08 14:37:06 +0300

    more tests

--- tasks/fibers/coroutine/tests/stacks.cpp
+++ tasks/fibers/coroutine/tests/stacks.cpp
@@ -5,7 +5,26 @@
 using mtf::fibers::Spawn;
 
 TEST_SUITE(Stacks) {
+
 #if !__has_feature(thread_sanitizer)
+
+  void Recurse(size_t left) {
+    mtf::fibers::Yield();
+
+    if (left > 0) {
+      Recurse(left - 1);
+    }
+  }
+
+  SIMPLE_TEST(Recurse) {
+    mtf::tp::StaticThreadPool scheduler{4};
+
+    for (size_t i = 0; i < 128; ++i) {
+      Spawn([]() { Recurse(128); }, scheduler);
+    }
+
+    scheduler.Join();
+  }
 
   TEST(Pool, wheels::test::TestOptions().TimeLimit(5s).AdaptTLToSanitizer()) {
     mtf::tp::StaticThreadPool scheduler{1};
