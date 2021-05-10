5d3ac2372587968e7e7d129dfe774670bb372cb6
Roman Lipovsky <roman.lipovsky@gmail.com>
2021-05-10 04:34:52 +0300

    better

--- tasks/stackless/gorroutines/play/main.cpp
+++ tasks/stackless/gorroutines/play/main.cpp
@@ -3,11 +3,13 @@
 #include <gorr/runtime/mutex.hpp>
 #include <gorr/runtime/join.hpp>
 
+#include <iostream>
+
 gorr::StaticThreadPool scheduler{/*threads=*/4};
 
 gorr::JoinHandle GorRoutine() {
   co_await scheduler.Schedule();
-  std::cout << "Hello, World!" << std::endl;
+  std::cout << "Hello, world!" << std::endl;
   co_return;
 };
 
