b2d5ff46f5f3343e1cd7b9ba17378b2bad47978e
Roman Lipovsky <roman.lipovsky@gmail.com>
2021-05-15 18:55:51 +0300

    better channel template

--- tasks/fibers/channel/mtf/fibers/sync/channel.hpp
+++ tasks/fibers/channel/mtf/fibers/sync/channel.hpp
@@ -6,6 +6,13 @@
 
 namespace mtf::fibers {
 
+namespace detail {
+
+template <typename X, typename Y>
+class Selector;
+
+}  // namespace detail
+
 // Buffered MP/MC channel
 // https://tour.golang.org/concurrency/3
 
@@ -16,9 +23,12 @@ template <typename T>
 class Channel {
   using Impl = detail::ChannelImpl<T>;
 
+  template <typename X, typename Y>
+  friend class detail::Selector;
+
  public:
   // Bounded channel, `capacity` > 0
-  Channel(size_t capacity) : impl_(std::make_shared<Impl>(capacity)) {
+  explicit Channel(size_t capacity) : impl_(std::make_shared<Impl>(capacity)) {
   }
 
   // Blocking
