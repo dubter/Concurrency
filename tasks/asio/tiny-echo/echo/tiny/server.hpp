858bf0b2fe68001f0e5a5cfcdfc5d5d12b2dafae
Roman Lipovsky <roman.lipovsky@gmail.com>
2023-02-04 17:13:44 +0300

    cleanup

--- tasks/asio/tiny-echo/echo/tiny/server.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <cstdint>
-
-namespace echo::tiny {
-
-void ServeForever(uint16_t port);
-
-}  // namespace echo::tiny
--- tasks/asio/tiny-echo/tinyfibers/api.hpp
+++
@@ -1,3 +0,0 @@
-#pragma once
-
-#include <tinyfibers/core/api.hpp>
