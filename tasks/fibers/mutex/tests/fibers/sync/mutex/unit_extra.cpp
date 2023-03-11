d981982dfaeafbac8ab3d633fd199816080590fa
Roman Lipovsky <roman.lipovsky@gmail.com>
2023-03-11 16:22:16 +0300

    fix test

--- tasks/fibers/mutex/tests/fibers/sync/mutex/unit_extra.cpp
+++ tasks/fibers/mutex/tests/fibers/sync/mutex/unit_extra.cpp
@@ -1,26 +1,23 @@
 #include <wheels/test/framework.hpp>
 
-#include <exe/tp/thread_pool.hpp>
+#include <exe/executors/thread_pool.hpp>
+#include <exe/fibers/sched/go.hpp>
 #include <exe/fibers/sync/mutex.hpp>
 
-#include <wheels/support/cpu_time.hpp>
-
-#include <atomic>
 #include <chrono>
 #include <thread>
 
-using namespace exe::tp;
-using namespace exe::fibers;
+using namespace exe;
 
 using namespace std::chrono_literals;
 
 TEST_SUITE(MutexExtra) {
   SIMPLE_TEST(UnlockFork) {
-    ThreadPool scheduler{4};
+    executors::ThreadPool scheduler{4};
 
-    Mutex mutex;
+    fibers::Mutex mutex;
 
-    Go(scheduler, [&]() {
+    fibers::Go(scheduler, [&] {
       mutex.Lock();
       std::this_thread::sleep_for(1s);
       mutex.Unlock();
@@ -29,7 +26,7 @@ TEST_SUITE(MutexExtra) {
     std::this_thread::sleep_for(128ms);
 
     for (size_t i = 0; i < 3; ++i) {
-      Go(scheduler, [&]() {
+      fibers::Go(scheduler, [&]() {
         mutex.Lock();
         mutex.Unlock();
         std::this_thread::sleep_for(5s);
