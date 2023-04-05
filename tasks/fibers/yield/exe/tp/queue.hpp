041adc2598bd13d352a5414a7a015c6862735071
dubter <dubenkov.rs@phystech.edu>
2023-04-05 21:03:05 +0300

    push

---
+++ tasks/fibers/yield/exe/tp/queue.hpp
@@ -0,0 +1,53 @@
+#pragma once
+
+#include <twist/ed/stdlike/mutex.hpp>
+#include <twist/ed/stdlike/condition_variable.hpp>
+
+#include <optional>
+#include <deque>
+
+namespace tp {
+
+// Unbounded blocking multi-producers/multi-consumers (MPMC) queue
+
+template <typename T>
+class UnboundedBlockingQueue {
+ public:
+  bool Put(T value) {
+    std::lock_guard lock(mutex_);
+    if (is_closed_) {
+      return false;
+    }
+
+    queue_.push_back(std::move(value));
+    can_take_.notify_one();
+    return true;
+  }
+
+  std::optional<T> Take() {
+    std::unique_lock lock(mutex_);
+    while (!is_closed_ && queue_.empty()) {
+      can_take_.wait(lock);
+    }
+    if (queue_.empty()) {
+      return std::nullopt;
+    }
+    T head = std::move(queue_.front());
+    queue_.pop_front();
+    return head;
+  }
+
+  void Close() {
+    std::lock_guard guard(mutex_);
+    is_closed_ = true;
+    can_take_.notify_all();
+  }
+
+ private:
+  bool is_closed_{false};
+  std::deque<T> queue_;
+  twist::ed::stdlike::condition_variable can_take_;
+  twist::ed::stdlike::mutex mutex_;
+};
+
+}  // namespace tp
---
+++ tasks/fibers/yield/exe/tp/wait_group.hpp
@@ -0,0 +1,40 @@
+#pragma once
+
+#include <twist/ed/stdlike/mutex.hpp>
+#include <twist/ed/stdlike/condition_variable.hpp>
+#include <twist/ed/wait/sys.hpp>
+#include <cstdlib>
+
+namespace tp::detail {
+class WaitGroup {
+ public:
+  // += count
+  void Add(size_t count) {
+    std::lock_guard lock(mutex_);
+    counter_ += count;
+  }
+
+  // =- 1
+  void Done() {
+    std::lock_guard lock(mutex_);
+    counter_--;
+    if (counter_ == 0) {
+      waiting_.notify_all();
+    }
+  }
+
+  // == 0
+  // One-shot
+  void Wait() {
+    std::unique_lock lock(mutex_);
+    while (counter_ != 0) {
+      waiting_.wait(lock);
+    }
+  }
+
+ private:
+  twist::ed::stdlike::mutex mutex_;
+  twist::ed::stdlike::condition_variable waiting_;
+  uint32_t counter_{0};
+};
+}  // namespace tp::detail
